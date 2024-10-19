6a2e430f5c87d0b856abb5895b7cc41c47fc5e6e
Nodari L <->
2024-10-19 18:57:31 +0200

    saving bookmarks and new folder

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const BASE_INDENT = 20; // indent in pixels
 let ACTIVE_TAB = {};
 
-const traverseBookmarks = (node, level) => {
-  let html = "";
-
+const buildBookmarksTree = (node, parent, level) => {
   if (!node.hasOwnProperty("children")) {
-    return html;
+    return 0;
   }
-  // console.log(`level ${level} node title ${node.title}`);
-  const indent = BASE_INDENT * level;
-  // <i class="fa-solid fa-angle-right">
-  html += `<div class="bookmark-folder level-${level}" style="padding-left:${indent}px;" data-id="${node.id}" data-parent-id="${node.parentId}">`
-  + `</i><i class="fa-regular fa-folder"></i>${node.title}`
-  + `</div>`;
+
+  const folderIcon = document.createElement("i"),
+        folder = document.createElement("div");
+
+  folderIcon.classList.add("fa-regular");
+  folderIcon.classList.add("fa-folder");
+  folder.appendChild(folderIcon);
+
+  folder.classList.add("bookmark-folder");
+  folder.classList.add(`level-${level}`);
+  folder.setAttribute("style", `padding-left:${BASE_INDENT*level}px;`);
+  folder.setAttribute("data-id", `${node.id}`);
+  folder.setAttribute("data-parent-id", `${node.parentId}`);
+  folder.setAttribute("data-level", `${level}`);
+  folder.innerText = node.title;
+
+  parent.appendChild(folder);
+
+  const divTreeContainer = document.createElement("div");
+  divTreeContainer.classList.add("bookmark-tree-container");
+  divTreeContainer.setAttribute("data-parent-id", node.id);
+  parent.appendChild(divTreeContainer);
 
   for (const child of node.children) {
-    html += traverseBookmarks(child, level +1);
+    buildBookmarksTree(child, divTreeContainer, level+1);
   }
 
-  return html;
+  return 1;
 };
 
 // Fetch current bookmarks
@@ -26,15 +40,23 @@ chrome.bookmarks.getTree((nodes) => {
   console.log("nodes", nodes, typeof nodes);
   const divBookmarksTree = document.querySelector("div#bookmarks-tree");
 
+  // const divTreeContainer = document.createElement("div");
+  // divTreeContainer.classList.add("bookmark-tree-container");
+  // divBookmarksTree.appendChild(divTreeContainer);
+
   let bookmarksTreeHTML = "";
   for (const child of nodes[0].children) {
-    bookmarksTreeHTML += traverseBookmarks(child, 0);
+    buildBookmarksTree(child, divBookmarksTree, 0);
   }
 
 
-  divBookmarksTree.innerHTML = bookmarksTreeHTML;
+  // divBookmarksTree.innerHTML = bookmarksTreeHTML;
 
   divBookmarksTree.addEventListener("click", (event) => {
+    console.log("event.srcElement.tagName", event.srcElement.tagName);
+    if (event.srcElement.tagName === "INPUT") {
+      return;
+    }
     const selectedFolder = document.querySelector("div.bookmark-folder-selected");
 
     if (selectedFolder) {
@@ -43,6 +65,9 @@ chrome.bookmarks.getTree((nodes) => {
 
     console.log(event);
     const element = event.srcElement;
+
+
+
     element.classList.add("bookmark-folder-selected");
     document.querySelector("input#tab-folder").value = event.srcElement.innerText;
   });
@@ -68,22 +93,72 @@ chrome.tabs.query({"active": true}, (tabs) => {
 });
 
 
-// Create a new folder
+// Show "Create a new folder" window
 document.querySelector("button#footer-btn-new-folder").addEventListener("click", (event) => {
+  const selectedFolder = document.querySelector("div.bookmark-folder-selected");
 
+  if (!selectedFolder) {
+    return;
+  }
+
+  selectedFolder.classList.remove("bookmark-folder-selected");
+
+  const selectedFolderId = parseInt(selectedFolder.getAttribute("data-id"));
+  const selectedFolderLevel = parseInt(selectedFolder.getAttribute("data-level"));
+  const parentDiv = document.querySelector(`div[data-parent-id="${selectedFolderId}"]`);
+
+  const newDiv = document.createElement("div"),
+        divLevel = selectedFolderLevel+1;
+
+  newDiv.classList.add("bookmark-folder-selected");
+  newDiv.setAttribute("style", `padding-left: ${BASE_INDENT*divLevel}`);
+  newDiv.setAttribute("data-parent-id", selectedFolderId);
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("value", "New Folder");
+  input.setAttribute("data-level", selectedFolderLevel+1);
+
+  newDiv.appendChild(input);
+  parentDiv.appendChild(newDiv);
 });
 
-
 // Save tab to the bookmarks
-
 document.querySelector("button#footer-btn-save").addEventListener("click", (event) => {
   const selectedFolder = document.querySelector("div.bookmark-folder-selected");
-  const parentId = selectedFolder.getAttribute("data-id");
-  console.log("parentId", parentId);
-  chrome.bookmarks.create({
-    'parentId': parentId,
-    'title': ACTIVE_TAB.title,
-    'url': ACTIVE_TAB.url,
-  });
+
+  if (!selectedFolder) {
+    return;
+  }
+
+  let selectedFolderId = selectedFolder.getAttribute("data-id");
+  let selectedFolderParentId = selectedFolder.getAttribute("data-parent-id");
+
+  console.log("selectedFolder.children", selectedFolder.children, selectedFolder.children.length);
+
+  if (selectedFolder.children.length >= 1) {
+    const selectedFolderInput = selectedFolder.children[0];
+    console.log("creating a folder", selectedFolderInput, selectedFolderInput.value)
+    chrome.bookmarks.create(
+      {"parentId": selectedFolderParentId, "title": selectedFolderInput.value},
+      function(newFolder) {
+        console.log("newFolder", newFolder);
+        selectedFolderId = newFolder.id;
+        chrome.bookmarks.create({
+          'parentId': newFolder.id,
+          'title': ACTIVE_TAB.title,
+          'url': ACTIVE_TAB.url,
+        });
+      },
+    );
+  } else {
+    console.log("selectedFolderId", selectedFolderId);
+    chrome.bookmarks.create({
+      'parentId': selectedFolderId,
+      'title': ACTIVE_TAB.title,
+      'url': ACTIVE_TAB.url,
+    });
+  }
+
   window.close();
 });
